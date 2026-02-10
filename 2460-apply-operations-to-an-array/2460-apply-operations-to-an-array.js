dd8160b8857bd353926ebad9762f52b75a6519e3
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-10 19:07:22 +0530

    Time: 0 ms (100%), Space: 58.1 MB (26.37%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var applyOperations = function(nums) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===nums[i+1]){
+            nums[i]=nums[i]*2;
+            nums[i+1]=0
+        }
+    }
+    let i=0;
+    for(let j=0;j<nums.length;j++){
+        if(nums[j]!==0){
+            [nums[i],nums[j]]=[nums[j],nums[i]];
+            i++;
+        }
+    }
+    return nums;
+};
