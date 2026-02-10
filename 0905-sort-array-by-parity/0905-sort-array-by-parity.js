7baf6612330651b7e0705e04ca2dae2786dafa97
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-10 16:25:16 +0530

    Time: 1 ms (85.57%), Space: 58.6 MB (30.53%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let left=0;
+    for(let right=0;right<nums.length;right++){
+        if(nums[right]%2===0){
+            [nums[left],nums[right]]=[nums[right],nums[left]];
+            left++;
+        }
+        
+    }
+    return nums;
+};
