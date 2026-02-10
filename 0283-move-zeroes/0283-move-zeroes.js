7961a783d5d0ec79200db9fb877b2836fd00fb82
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-10 15:45:43 +0530

    Time: 4 ms (29%), Space: 60.3 MB (59.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,11 +4,15 @@
  */
 var moveZeroes = function(nums) {
     let i=0,j=0;
+    // run the loop from j=0 to nums.length
     while(j<nums.length){
+        // check if the current value is not a zero and then swap with ith element 
         if(nums[j]!==0){
+            // swap here and after swap we need to increment the value of i
             [nums[i],nums[j]]=[nums[j],nums[i]];
             i++;
         }
+        // we increment the value of j everytime until the loop ends.
         j++;
     }
     return nums;
