bccb4ebc7ca1188063aba0fbfc2d2f4f38915a08
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-10 15:41:34 +0530

    Time: 0 ms (100%), Space: 55.1 MB (8.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,8 +5,11 @@
  */
 var removeElement = function(nums, val) {
     let i=0,j=0;
+    // iterate from j=0 to nums.length
     while(j<nums.length){
+        // check whether the current element is equals to val if not push it into the nums[i];
         if(nums[j]!==val){
+            // then the unique value will be stored inside nums[i];
             nums[i]=nums[j];
             i++;
         }
