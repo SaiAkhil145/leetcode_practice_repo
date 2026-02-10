e14cb7bd70042ee418011ebb9ad9831dc70a74f9
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-10 19:33:09 +0530

    Time: 1 ms (58.80%) | Memory: 54.5 MB (63.73%) - LeetSync

---
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function(nums, original) {
+    while(nums.find(val=>val===original)){
+        original*=2;
+    }
+    return original;
+};
