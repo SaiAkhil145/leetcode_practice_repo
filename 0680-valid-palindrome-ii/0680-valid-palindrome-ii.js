3282e97c84dc80e08cf29dbe5739503f26530472
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-11 14:26:35 +0530

    Time: 4 ms (93.24%), Space: 59.8 MB (97.23%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -3,19 +3,19 @@
  * @return {boolean}
  */
 var validPalindrome = function(s) {
-    let start = 0,end=s.length-1;
-    while(start<end){
+    let start=0,end=s.length-1;
+    while(start<=end){
         if(s[start]!==s[end]){
             return isPal(s,start+1,end) || isPal(s,start,end-1);
         }
         start++;
-        end--;
+        end--
     }
     return true;
 };
 
 var isPal = function(s,start,end){
-    while(start<end){
+    while(start<=end){
         if(s[start]!==s[end]){
             return false;
         }
