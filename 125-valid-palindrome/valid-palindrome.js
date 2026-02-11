8fd6d5c7241f0848405f55e41cb579db18942a78
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-11 12:14:38 +0530

    Time: 4 ms (76.22%) | Memory: 56.5 MB (85.24%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,30 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let clean  = cleanStr(s);
-    return isPal(clean);
+    let cleaned = s.toLowerCase().replace(/[^a-z0-9]/g,"");
+    const reversed = cleaned.split('').reverse().join('');
+    return cleaned===reversed
 };
-
-var cleanStr = function(s){
-    let chars ="abcdefghijklmnopqrstuvwxyz0123456789";
-    let res ="";
-    for(let ch of s){
-        let c = ch.toLowerCase();
-        if(chars.indexOf(c)!==-1){
-            res+=c;
-        }
-    }
-    return res;
-}
-
-var isPal = function(s){
-    let start=0,end=s.length-1;
-    while(start<=end){
-        if(s[start]!==s[end]){
-            return false;
-        }
-        start++;
-        end--;
-    }
-    return true;
-}
