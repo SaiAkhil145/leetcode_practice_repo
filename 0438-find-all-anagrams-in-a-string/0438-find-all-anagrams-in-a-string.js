166b949428de5c4b0422bafa9c3d3cfb43144dfa
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-11 15:41:45 +0530

    Time: 12 ms (84.12%), Space: 59.9 MB (70.74%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -5,30 +5,26 @@
  */
 var findAnagrams = function(s, p) {
     let left=0;
-    let valid =0;
-    let res = [];
+    let result=[];
+    let valid=0;
     let need = new Map();
-    // making frequancy map for p values;
     for(let ch of p){
         need.set(ch,(need.get(ch) || 0)+1);
     }
-    // iterating the s string from start to end.
-    // map for storing anagram values in s
     let window = new Map();
     for(let right=0;right<s.length;right++){
         let c = s[right];
+
         if(need.has(c)){
             window.set(c,(window.get(c) || 0)+1);
-            // increment the valid count if the frequancy of value in window matches with need map.
             if(window.get(c)===need.get(c)){
                 valid++;
             }
         }
-
-        // shrink
+        // shrinking
         while(right-left+1>=p.length){
             if(valid===need.size){
-                res.push(left);
+                result.push(left);
             }
             let d = s[left];
             left++;
@@ -36,12 +32,9 @@ var findAnagrams = function(s, p) {
                 if(need.get(d)===window.get(d)){
                     valid--;
                 }
-                window.set(d,(window.get(d)-1));
+                window.set(d,window.get(d)-1);
             }
         }
-        
-
     }
-    return res;
-    
+    return result;
 };
