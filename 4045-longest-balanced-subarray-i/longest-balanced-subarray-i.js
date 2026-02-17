df00fa09ba47e061103ee38b2c533bcac40ce62b
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-17 22:05:58 +0530

    Time: 276 ms (48.61%) | Memory: 60 MB (87.14%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.js
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.js
@@ -5,20 +5,19 @@
 var longestBalanced = function(nums) {
     let maxLen=0,n=nums.length;
     for(let i=0;i<n;i++){
-        let seen = new Set();
-        let even=0,odd=0;
+        let evenSet = new Set();
+        let oddSet = new Set();
         for(let j=i;j<n;j++){
-            if(!seen.has(nums[j])){
-                seen.add(nums[j]);
-            
-            if(nums[j]%2===0) even++;
-            else odd++;
+            let num = nums[j];
+            if(num%2===0){
+                evenSet.add(num);
+            }else{
+                oddSet.add(num);
             }
-            if(even===odd){
-            maxLen=Math.max(maxLen,j-i+1);
+            if(evenSet.size===oddSet.size){
+                maxLen=Math.max(maxLen,j-i+1);
+            }
         }
-        }
-        
     }
     return maxLen;
 };
