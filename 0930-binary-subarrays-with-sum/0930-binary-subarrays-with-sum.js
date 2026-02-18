675b0ad7b8b81656bae22671adf8f92787685b05
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-18 11:05:21 +0530

    Time: 21 ms (9.34%), Space: 62.5 MB (18.13%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
@@ -4,20 +4,16 @@
  * @return {number}
  */
 var numSubarraysWithSum = function(nums, goal) {
-    return validSubarray(nums,goal)-validSubarray(nums,goal-1);
-};
-
-var validSubarray = function(nums,goal){
-    if(goal<0) return 0;
+    let map = new Map();
     let sum=0,count=0;
-    let left=0;
-    for(let right=0;right<nums.length;right++){
-        sum+=nums[right];
-        while(sum>goal){
-            sum-=nums[left];
-            left++;
+    map.set(0,1);
+    for(let num of nums){
+        sum+=num;
+        let diff = sum-goal;
+        if(map.has(diff)){
+            count+=map.get(diff);
         }
-        count+=right-left+1;
+        map.set(sum,(map.get(sum) || 0)+1);
     }
     return count;
-}
+};
