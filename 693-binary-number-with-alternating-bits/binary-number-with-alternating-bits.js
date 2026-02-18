96358dc8cc4eb787673e07c3bae24fb481e6f75a
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-02-18 09:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (83.33%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function(n) {
+    let prev = n&1;
+    n=n>>1;
+    while(n>0){
+        let curr = n&1;
+        if(curr===prev) return false;
+
+        prev=curr;
+        n=n>>1;
+    }
+    return true;
+};
