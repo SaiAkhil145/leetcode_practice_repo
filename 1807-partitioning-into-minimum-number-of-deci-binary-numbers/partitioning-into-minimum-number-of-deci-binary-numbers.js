b57963c3552f35659d92e93aaeb8e63ed6cb487d
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-01 10:44:18 +0530

    Time: 18 ms (67.21%) | Memory: 59 MB (83.61%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.js
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -5,7 +5,10 @@
 var minPartitions = function(n) {
     let maxNum = 0;
     for(let ch of n){
-        maxNum = Math.max(Number(ch),maxNum);
+        let c = Number(ch);
+        if(c>maxNum){
+            maxNum=c;
+        }
 
         if(ch===9) return 9;
     }
