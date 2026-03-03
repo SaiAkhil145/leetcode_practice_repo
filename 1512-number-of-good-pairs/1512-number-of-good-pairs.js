94d59ce8ab4144c430dec2094cc4ed9130c5f396
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-03 22:38:02 +0530

    Time: 0 ms (100%), Space: 53 MB (91.91%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let map = {};
+    let count=0;
+    for(let num of nums){
+        if(map[num]){
+            count+=map[num];
+            map[num]+=1;
+        }else{
+            map[num]=1;
+        }
+    }
+    return count;
+};
