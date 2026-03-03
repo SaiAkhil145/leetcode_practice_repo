83e44a6ea91a6ad3c5635f1af46919c02eeb8052
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-03 12:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (56.99%) - LeetSync

--- 394-decode-string/decode-string.js
+++ 394-decode-string/decode-string.js
@@ -3,19 +3,20 @@
  * @return {string}
  */
 var decodeString = function(s) {
-    let stk = [];
+    let stk=[];
     let currString="",currNumber=0;
     for(let char of s){
         if(!isNaN(char)){
-            currNumber = currNumber*10+Number(char);
+            currNumber=currNumber*10+Number(char);
         }else if(char==='['){
             stk.push([currString,currNumber]);
-            currString="";
             currNumber=0;
-        }else if(char==="]"){
-            let [prevString,num]=stk.pop();
+            currString="";
+        }else if(char===']'){
+            let [prevString,num] = stk.pop();
             currString = prevString+currString.repeat(num);
-        }else{
+        }
+        else{
             currString+=char;
         }
     }
