4f23a783861242e68dbd4c45774c09446894c2f3
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-03 12:56:44 +0530

    Time: 44 ms (16.78%) | Memory: 67.2 MB (13.95%) - LeetSync

--- 224-basic-calculator/basic-calculator.js
+++ 224-basic-calculator/basic-calculator.js
@@ -3,5 +3,33 @@
  * @return {number}
  */
 var calculate = function(s) {
-    return eval(s);
+    let stk=[];
+    let result=0;
+    let sign=1;
+    let number=0;
+    for(let char of s){
+        if(!isNaN(char) &&  char!==' '){
+            number = number*10+Number(char);
+        }else if(char==="+"){
+            result+=sign*number;
+            number=0;
+            sign=1;
+        }else if(char==='-'){
+            result+=sign*number;
+            number=0;
+            sign=-1;
+        }else if(char==='('){
+            stk.push(result);
+            stk.push(sign);
+            result=0;
+            sign=1;
+        }else if(char===')'){
+            result+=sign*number;
+            number=0;
+            result*=stk.pop();
+            result+=stk.pop();
+        }
+
+    }
+    return result+sign*number;
 };
