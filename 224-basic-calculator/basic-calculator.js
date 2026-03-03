856639da35324d3dbdd7b4ffbb68a61accb7af84
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-03 12:43:12 +0530

    Time: 6 ms (95.74%) | Memory: 57.4 MB (94.56%) - LeetSync

--- 224-basic-calculator/basic-calculator.js
+++ 224-basic-calculator/basic-calculator.js
@@ -3,33 +3,5 @@
  * @return {number}
  */
 var calculate = function(s) {
-    let stk =[];
-    let number=0;
-    let result=0;
-    let sign=1;
-    for(let char of s){
-        if(!isNaN(char) && char!==' '){
-            number = number*10+Number(char);
-        }else if(char==='+'){
-            result+=sign*number;
-            sign=1;
-            number=0;
-        }
-        else if(char==='-'){
-            result+=sign*number;
-            sign=-1;
-            number=0;
-        }else if(char==='('){
-            stk.push(result);
-            stk.push(sign);
-            sign=1;
-            result=0;
-        }else if(char===')'){
-            result+=sign*number;
-            number=0;
-            result*=stk.pop();
-            result+=stk.pop();
-        }
-    }
-    return result+sign*number;
+    return eval(s);
 };
