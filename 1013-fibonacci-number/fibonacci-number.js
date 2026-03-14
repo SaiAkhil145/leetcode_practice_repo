697f00db0ae26cda45b3e6c9ddd8837c63db7c93
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-14 15:47:50 +0530

    Time: 49 ms (61.10%) | Memory: 53.6 MB (63.19%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -2,7 +2,13 @@
  * @param {number} n
  * @return {number}
  */
+ let store = {};
 var fib = function(n) {
-    if(n<=1) return n;
-    return fib(n-1)+fib(n-2);
+    if(n<=1){
+        return n;
+    }
+    if(!store[n]){
+        store[n]=fib(n-1)+fib(n-2);
+    }
+    return store[n];
 };
