121b9fc6d636db25c03ebdaf38a329b79fb8500a
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-16 20:09:58 +0530

    Time: 20 ms (68.26%) | Memory: 55.9 MB (61.47%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    function expand(left,right){
+        while(left>=0 && right<s.length && s[left]===s[right]){
+            left--;
+            right++;
+        }
+        return s.substring(left+1,right);
+    }
+    let longest="";
+    for(let i=0;i<s.length;i++){
+        let odd = expand(i,i);
+        let even = expand(i,i+1);
+        if(even.length>longest.length){
+            longest = even;
+        }
+        if(odd.length>longest.length){
+            longest=odd;
+        }
+    }
+    return longest;
+
+};
