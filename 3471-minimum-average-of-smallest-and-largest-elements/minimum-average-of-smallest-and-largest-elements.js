b1e08bab1a750af38d57023dd6514bb612ea2d79
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-18 18:23:30 +0530

    Time: 2 ms (69.18%) | Memory: 58 MB (5.03%) - LeetSync

---
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumAverage = function(nums) {
+    nums.sort((a,b)=>a-b);
+    let j = nums.length-1;
+    let minAvg = Infinity;
+    for(let i=0;i<Math.floor(nums.length/2);i++){
+        let avg = (nums[i]+nums[j])/2;
+        minAvg = Math.min(minAvg,avg);
+        j--;
+    }
+    return minAvg;
+};
