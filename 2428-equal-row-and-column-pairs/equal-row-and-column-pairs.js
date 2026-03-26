08954b095b7b5a8c28a9df15cc6e8414b3aa9ebb
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-03-26 21:50:00 +0530

    Time: 27 ms (83.90%) | Memory: 62.5 MB (94.19%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var equalPairs = function(grid) {
+    const n = grid.length;
+    let map = new Map();
+    for(let row of grid){
+        let key = row.join(',');
+        map.set(key,(map.get(key) || 0)+1);
+    }
+    let count=0;
+    for(let col=0;col<n;col++){
+        let arr = [];
+        for(let row=0;row<n;row++){
+            arr.push(grid[row][col]);
+        }
+        let key = arr.join(',');
+        if(map.has(key)){
+            count+=map.get(key);
+        }
+    }
+    return count;
+};
