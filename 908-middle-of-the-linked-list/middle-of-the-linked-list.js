c5e4510a336f8cdab2bb26c288d231a04d756892
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-04-03 12:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (46.01%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.js
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -11,7 +11,7 @@
  */
 var middleNode = function(head) {
     let slow=head;
-    let fast = head;
+    let fast=head;
     while(fast && fast.next){
         slow=slow.next;
         fast=fast.next.next;
