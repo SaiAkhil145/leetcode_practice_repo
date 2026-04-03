af68054c9717e6eddd3d6fbdff96a98741f716bd
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-04-03 11:47:40 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (15.96%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -11,7 +11,7 @@
  */
 var reverseList = function(head) {
     let prev=null;
-    let curr = head;
+    let curr=head;
     while(curr!==null){
         let next = curr.next;
         curr.next = prev;
