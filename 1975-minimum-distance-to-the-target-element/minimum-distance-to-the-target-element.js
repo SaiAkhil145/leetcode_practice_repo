08b96e58e982395abd9e2f68949d92019f2fbd5e
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2026-04-13 15:09:30 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (42.86%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
+var getMinDistance = function(nums, target, start) {
+     const n = nums.length;
+    
+    for (let d = 0; d < n; d++) {
+        if (start - d >= 0 && nums[start - d] === target)
+            return d;
+        
+        if (start + d < n && nums[start + d] === target)
+            return d;
+    }
+    
+    return -1;
+};
